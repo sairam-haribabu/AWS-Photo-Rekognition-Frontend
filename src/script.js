a942c0a17ebc17de89ee419383e1c7b14de46879
saihari22 <89160461+saihari22@users.noreply.github.com>
2022-03-24 17:44:27 -0400

    UI Changes

--- src/script.js
+++ src/script.js
@@ -2,41 +2,53 @@ var sdk = apigClientFactory.newClient({apiKey:'ZwEjIsMQNG18h2QXr2H3c3XIOVlwSmaT7
 
 
 function searchPhotos() {
-    // q = $("#query").val();
+    document.getElementById('resultsID').innerHTML="";
     q = document.getElementById('query').value
-    console.log(q);
+    console.log(q)
     sdk.searchGet({'query':q}, {}, {}).then((response) => {
-        console.log(response);
         var images = response.data.body;
-
-        var images = response.data.body;
-
-        var images_div = document.getElementById('search_result');
+        var images_div = document.getElementById('resultsID');
         while (images_div.firstChild){
             images_div.removeChild(images_div.firstChild);
         }
         if(images.length == 0){
-            
-            var text = document.createElement('h3');
-            text.textContent = 'No images!!';
-            console.log('here!');
-            document.getElementById('search_result').appendChild(text);
+            var text = document.createElement('h2');
+            text.textContent = 'No Images found';
+            document.getElementById('resultsID').appendChild(text);
         }
         else{
+            var text = document.createElement('h2');
+            text.textContent = 'Here are your results!';
+            document.getElementById('resultsID').appendChild(text);
             for(var image in images){
                 var image_element = document.createElement('img');
                 image_element.src = images[image];
-                image_element.width = '250';
-                image_element.height = '250';
+                image_element.width = '200';
+                image_element.height = '200';
                 image_element.className = 'thumbnail';
-                
-                document.getElementById('search_result').appendChild(image_element);
+                document.getElementById('resultsID').appendChild(image_element);
             }
         }
+        document.getElementById('query').value='';
     })
+
     .catch((error) => {
         console.log('an error occurred', error);
     });
+}
+
+function record(){    
+    window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+    const recognition = new window.SpeechRecognition();
+
+    recognition.onresult = (event) => {
+        const speechToText = event.results[0][0].transcript;
+        document.getElementById('query').value = speechToText;
+        searchPhotos();
+        }
+
+    recognition.stop();
+    recognition.start();
 }
 
 
@@ -57,16 +69,13 @@ function getBase64(file) {
 
 
 function uploadPhotos() {
-    var image = document.getElementById('picture').files[0];
-    // var label = $("#label").val()
+    var image = document.getElementById('inputFile').files[0];
     var label=document.getElementById('label').value
     var params = {"item" : image.name, "bucket" : "ccbd-photos-bucket", "x-amz-meta-customLabels": label, "Content-Type": "image/jpeg"};
     var encoded_image = getBase64(image).then(
         data => {
         sdk.uploadBucketItemPut(params, data , {}).then(function(res){
            window.alert("Uploaded " + image.name);
-        //    $("#label").val("")
-        //    $("#picture").val("")
             console.log(res)
         })
         .catch((error) => {
@@ -86,17 +95,16 @@ function uploadPhoto(){
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
             console.log(this.responseText);
+            console.log(this.status)
         }
     });
     xhr.withCredentials = false;
     let urlcreate="https://lwf6skoai1.execute-api.us-east-1.amazonaws.com/delta/upload/"+bucketname+"/"+data.name;
-    //let urlcreate="https://n4dvgpnjd7.execute-api.us-east-1.amazonaws.com/gamma/upload/"+bucketname+"/"+data.name;
-
     xhr.open("PUT",urlcreate);
     xhr.setRequestHeader("Content-Type", data.type);
     xhr.setRequestHeader("x-amz-meta-customlabel", labels );
     xhr.setRequestHeader("x-api-key", 'ZwEjIsMQNG18h2QXr2H3c3XIOVlwSmaT7jNyYCAA' );
     xhr.send(data);
     console.log("success")
-    window.alert("Uploaded " + data.name);
+    alert("Success uploading")
 }
